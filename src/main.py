4966966c6f686e0966a2a902dd196269f57e8cdd
Ahmer-Jadoon-code <ahmer.jadoon786@gmail.com>
2026-08-17 04:34:49 +0000

    Project code

---
+++ src/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from datetime import datetime
+
+app = FastAPI(title="Smart City AQI API")
+
+
+@app.get("/health")
+def health_check():
+    return {
+        "status": "ok",
+        "service": "Smart City AQI API",
+        "timestamp": datetime.utcnow().isoformat()
+
+    }
+
+
+
